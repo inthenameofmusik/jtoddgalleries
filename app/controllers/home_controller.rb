24e908ac805057e93a038c56aaae00a9e6bda6de
Niles Brandon <nilesmbrandon1@icloud.com>
2015-07-22 02:21:10 -0400

    fixed home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,13 @@ class HomeController < ApplicationController
   def index
   	@first_one = Exhibition.order('from_date DESC').first
 
-	if (@first_one.from_date..@first_one.to_date).cover?(DateTime.now.to_date)
-		@correct_version = "Now Showing"
-	else
-		@correct_version = "Soon Showing"
-	end
+	# if (@first_one.from_date..@first_one.to_date).cover?(DateTime.now.to_date)
+	# 	@correct_version = "Now Showing"
+	# else
+	# 	@correct_version = "Soon Showing"
+	# end
+
+	@correct_version = "Now Showing"
 	
   	@full_feature = false
   end
