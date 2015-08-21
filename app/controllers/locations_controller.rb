b74917066704ad4e1988b5014e4da369709a85ec
Niles Brandon <nilesmbrandon1@icloud.com>
2015-08-21 01:09:41 -0400

    fixed trial sub-heading, artist text

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,10 @@
 class LocationsController < ApplicationController
+	before_filter :set_location, only: [:edit, :update]
+
 	def index
 		@location = Location.order("lower(title) ASC").all
 		@new_location = Location.new
+		@full_feature = true
 	end
 
 	def create
@@ -16,6 +19,18 @@ class LocationsController < ApplicationController
 		end
 	end
 
+	def edit
+		@full_feature = true
+	end
+
+	def update
+		if @location.update(location_params)
+			redirect_to action: 'index'
+		else
+			redirect_to action: 'edit'
+		end
+	end
+
 	def destroy
 		@location = Location.find(params[:id])
 		@location.destroy
@@ -24,6 +39,10 @@ class LocationsController < ApplicationController
   	end
 
 	private
+
+	def set_location
+		@location = Location.find(params[:id])
+	end
 
 	def location_params
 		params.require(:location).permit(:title)
